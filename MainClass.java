703f543425335c3cfe899268447ac46302ff0731
shezan7 <46520348+shezan7@users.noreply.github.com>
2019-04-16 12:42:13 +0600

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -12,7 +12,18 @@ public class MainClass {
 		Scanner scan = new Scanner(System.in);
 		FileWriters fwc = new FileWriters();
 		
+		//System.out.println("Enter your choice:\n1.See Details of A Student\n2.See Details of All Students\n3.Insert a Student's Information\n4.Delete a Student's Information");
+		//int n = scan.nextInt();
+		
 		fwc.writeIntoFile();
+		
+		//oc.printAllStudentInfo();
+		//fwc.writeIntoFile();
+		
+		//if(n==1) oc.printStudentInfo();
+		//else if(n==2) oc.printAllStudentInfo();
+		//else if(n==3) fwc.writeIntoFile();
+		//else if(n==4) oc.deleteStudent();	
 		
 		scan.close();
 	}
