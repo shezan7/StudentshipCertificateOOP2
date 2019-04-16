b5ef753ec39d60e75268fb84c7a3d8dc182965f4
shezan7 <46520348+shezan7@users.noreply.github.com>
2019-04-16 11:22:49 +0600

    Add files via upload

---
+++ ClientWithIP.java
@@ -0,0 +1,39 @@
+package withIP;
+
+import java.io.*;
+import java.net.*;
+public class ClientWithIP
+{
+    PrintStream streamToServer;
+    BufferedReader streamFromServer;
+    Socket toServer;
+    public ClientWithIP()
+    {
+    	System.out.println("Welcome to Client");
+        connectToServer();
+    }
+    private void connectToServer()
+    {
+        try{
+            String name;
+            toServer = new Socket("10.100.100.167",30001);
+            streamFromServer = new BufferedReader(new InputStreamReader((toServer.getInputStream())));
+            streamToServer = new PrintStream(toServer.getOutputStream());
+            System.out.println("Enter Connection Name");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            
+            name = reader.readLine();   
+            streamToServer.println(name);
+            String str = streamFromServer.readLine();
+            System.out.println("The Server Says "+str);         
+        }
+        catch(Exception e)
+        {
+                System.out.println("Exception "+e);
+        }       
+    }
+    public static void main(String args[])
+    {
+        new ClientWithIP();
+    }
+}
